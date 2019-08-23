db4aaa794bd29ab855e297b3070705f8c9e4ff0b
Gard Sandholt <gard.sandholt@gmail.com>
2019-08-23 11:06:00 +0200

    Added main controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'main/login'
+  get 'main/playlist'
+  get 'main/start'
+  get 'main/quiz'
+  get 'main/summary'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'main#login'
 end
