5f2f602c16c550e6ac3ad2bed9b6d8f754320329
Gard Sandholt <gard.sandholt@gmail.com>
2019-08-23 11:21:25 +0200

    Refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'main/login'
-  get 'main/playlist'
-  get 'main/start'
-  get 'main/quiz'
-  get 'main/summary'
+  get 'login', to: 'main#login'
+  get 'playlist', to: 'main#playlist'
+  get 'start', to: 'main#start'
+  get 'quiz', to: 'main#quiz'
+  get 'summary', to: 'main#summary'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'main#login'
